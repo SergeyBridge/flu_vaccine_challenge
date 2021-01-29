355ea8c6130ab4b844cc7d6d6de4feee5c287b8c
SergeyBridge <sergey.novozhilov@yandex.ru>
2021-01-29 09:24:10 +0000

    commit with cross-validation StratifiedKFold AUC=0.8605

--- drivendata_flu_shot/config.py
+++ drivendata_flu_shot/config.py
@@ -1,26 +1,29 @@
+from catboost.utils import get_gpu_device_count
+
 params = {
     'loss_function': 'Logloss',
     'eval_metric': 'AUC',
     'thread_count': -1,
     'custom_metric': 'AUC:hints=skip_train~false',
-    'task_type': 'CPU', #  if get_gpu_device_count() > 0 else 'CPU',
+    'task_type': 'CPU' if get_gpu_device_count() > 0 else 'CPU',
     # 'task_type': 'GPU', # if torch.cuda.is_available() else 'CPU',
     'grow_policy': 'Lossguide',   # 'SymmetricTree',  #  'Depthwise',
-    # 'auto_class_weights': None,
+    'auto_class_weights': 'Balanced',
     # 'langevin': True,  # CPU only
-    'iterations': 250,
-    'learning_rate': 4e-3,
+    'iterations': 9000,
+    'learning_rate': 0.002,   # 4e-3,
     'l2_leaf_reg': 1e-1,
     'depth': 16,
     'max_leaves': 10,
     'border_count': 128,
-    'verbose': 500,
+    'verbose': 1000,
     'od_type': 'Iter',
     'od_wait': 100,
+    # 'early_stopping_rounds': 100,
 
     # random control
     'bootstrap_type': 'Bayesian',
-    # 'random_seed': 0,
+    # 'random_seed': 100,
     'random_strength': 0.001,
     'rsm': 1,
     'bagging_temperature': 0,
